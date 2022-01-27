cd8532bfd6dc90ec7a0ca568af48df5048a8c6d3
Rajkumar Rangaraj <rajrang@microsoft.com>
2022-01-27 11:46:29 +0530

    Save work in progress.

--- sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/LogsHelper.cs
+++ sdk/monitor/Azure.Monitor.OpenTelemetry.Exporter/src/LogsHelper.cs
@@ -2,6 +2,9 @@
 // Licensed under the MIT License.
 
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Reflection;
 using Azure.Monitor.OpenTelemetry.Exporter.Models;
 using Microsoft.Extensions.Logging;
 using OpenTelemetry;
@@ -17,9 +20,44 @@ namespace Azure.Monitor.OpenTelemetry.Exporter
         {
             List<TelemetryItem> telemetryItems = new List<TelemetryItem>();
             TelemetryItem telemetryItem;
+            string problemId;
+            string methodName = "UnknownMethod";
+            int methodOffset = System.Diagnostics.StackFrame.OFFSET_UNKNOWN;
 
             foreach (var logRecord in batchLogRecord)
             {
+                if (logRecord.Exception != null)
+                {
+                    var exceptionType = logRecord.Exception.GetType().FullName;
+                    var strackTrace = new StackTrace(logRecord.Exception);
+                    var exceptionStackFrame = strackTrace.GetFrame(1);
+
+                    if (exceptionStackFrame != null)
+                    {
+                        MethodBase methodBase = exceptionStackFrame.GetMethod();
+
+                        if (methodBase != null)
+                        {
+                            methodName = (methodBase.DeclaringType?.FullName ?? "Global") + "." + methodBase.Name;
+                            methodOffset = exceptionStackFrame.GetILOffset();
+                        }
+                    }
+
+                    if (methodOffset == System.Diagnostics.StackFrame.OFFSET_UNKNOWN)
+                    {
+                        problemId = exceptionType + " at " + methodName;
+                    }
+                    else
+                    {
+                        problemId = exceptionType + " at " + methodName + ":" + methodOffset.ToString(CultureInfo.InvariantCulture);
+                    }
+
+                    TelemetryExceptionDetails t = new TelemetryExceptionDetails(logRecord.State.ToString());
+                    t.Stack = logRecord.Exception.StackTrace;
+                    t.TypeName = logRecord.Exception.GetType().FullName;
+                    t.HasFullStack = logRecord.Exception.StackTrace != null;
+                }
+
                 telemetryItem = new TelemetryItem(logRecord);
                 telemetryItem.InstrumentationKey = instrumentationKey;
                 telemetryItem.SetResource(roleName, roleInstance);
